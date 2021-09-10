473963be63e875df0abb805f3bf9ebbb64727648
wsy <823391837@qq.com>
2021-09-10 18:29:28 +0800

    openlayer

--- eslintrc.js
+++ eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-extra-boolean-cast': 'off',
+        'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
     },
 }
